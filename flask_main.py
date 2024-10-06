333c6aac08ff86eca3c085121168c763dde01943
jorgeav527 <jorgeav527@gmail.com>
2024-10-06 10:18:52 -0500

    feat: delete one post

--- flask_main.py
+++ flask_main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, abort
 
 def get_db_connection():
     conn = sqlite3.connect("database.db")
@@ -43,7 +43,36 @@ def create_one_post():
     if request.method == "GET":
         return render_template("post/create.html")
 
+
+@app.route("/post/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for("get_all_post"))
+
+    if request.method == "GET":
+        return render_template("post/edit.html", post=post)
     
+
+@app.route('/post/delete/<int:post_id>', methods=['POST'])
+def delete_one_post(post_id):
+    # Obtener el post a eliminar basado en su post_id
+    conn = get_db_connection()
+    # Eliminar el post de la base de datos
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('get_all_post'))
 
 if __name__ == '__main__':
     app.run(debug=True)
